96b3fab1f2c2dea1ba20108b080f41965c0d28fc
richnou <leys.richard@gmail.com>
2016-05-23 16:09:17 +0200

    Added bash script to install odfi before generating code. Should fix CI

---
+++ src/odfi/load_odfi.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+loc=$(dirname "$(readlink -f ${BASH_SOURCE[0]})")
+
+## Make sure we have TCL
+#########
+sudo aptitude install tcl8.5 tcl8.6 itcl3
+
+## make sure we have ODFI 
+########
+if [[ ! -d $loc/../../target/odfi ]] 
+then
+	git clone https://github.com/richnou/odfi-manager.git $loc/../../target/odfi
+	source $loc/../../target/odfi/setup.linux.bash
+	
+
+else
+	p=$(pwd)
+	cd $loc/../../target/odfi
+	git pull 
+	cd $p
+fi
+
+source $loc/../../target/odfi/setup.linux.bash
+odfi --install local.dev-tcl
+odfi --update
+## REload to make sure new packages are loaded
+source $loc/../../target/odfi/setup.linux.bash
