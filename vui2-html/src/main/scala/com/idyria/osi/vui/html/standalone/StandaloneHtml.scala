c1d9a1345baa5dfc7c6ca4ebcb817d6df3ef890f
richnou <leys.richard@gmail.com>
2017-01-08 14:50:52 +0100

    2.0.0 Hotfix for open JDK

--- vui2-html/src/main/scala/com/idyria/osi/vui/html/standalone/StandaloneHtml.scala
+++ vui2-html/src/main/scala/com/idyria/osi/vui/html/standalone/StandaloneHtml.scala
@@ -23,41 +23,7 @@ package com.idyria.osi.vui.html.standalone
     //----------------------
     // Imported from E:/Common/Projects/git/vui2/vui2-html/src/gen/StandaloneHtml.body.scala
 
-// Engine Reference
-var engine: Option[com.idyria.osi.vui.html.js.JSEngine] = None
 
-var actions = Map[String, Any => Any]()
-
-// Register to JS Engine
-
-def registerClosure(cl: Any => Any) = {
-
-  // Id 
-  var id = cl.hashCode().toHexString
-  actions = actions + (id -> cl)
-
-  id
-}
-def getClosure(code:String) : Option[Any => Any] = {
-  
-  this.actions.get(code)
-}
-
-def call(code: String, param: AnyRef) = {
-
-  println(s"Calling for action $code, contained:  ${actions.contains(code)}")
-  //println(s"Calling with ${param.toString()}")
-  actions.get(code) match {
-    case Some(action) => action(param)
-    case None => throw new RuntimeException("Cannot find action to call");
-  }
-
-}
-
-
-def drop(e: netscape.javascript.JSObject) = {
-  println(s"Calling Drop: " + e.getMember("dataTransfer").asInstanceOf[netscape.javascript.JSObject].call("getData", "text"))
-}
 
 }
 
